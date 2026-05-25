abcb2f89ca7a3d2d5c22dc6466defd9e5c7147b1
Alexandru <lexyus22@gmail.com>
2026-05-25 12:23:05 +0200

    Add player controls and second paddle

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ screen.setup(width = 1000, height = 800)
 screen.bgcolor('black')
 screen.title('Pong')
 screen.tracer(0)
+screen.listen()
 
+paddle_1 = Paddle((470,0))
+screen.onkey(paddle_1.up, 'Up')
+screen.onkey(paddle_1.down, 'Down')
 
-paddle_1 = Paddle()
+paddle_2 = Paddle((-470,0))
+screen.onkey(paddle_2.up, 'w')
+screen.onkey(paddle_2.down, 's')
+
 
 
 screen.update()
--- paddle.py
+++ paddle.py
@@ -3,16 +3,28 @@ import turtle as t
 
 STRETCH_WID = 5
 STRETCH_LEN = 1
-X_POS = 470
-Y_POS = 0
+
+MOVE = 20
 
 class Paddle(t.Turtle):
 
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.shape('square')
         self.color('white')
         self.shapesize(STRETCH_WID, STRETCH_LEN)
         self.speed('fastest')
         self.penup()
-        self.goto(X_POS, Y_POS)
+        self.goto(position)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+        self.screen.update()
+
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+        self.screen.update()
+
